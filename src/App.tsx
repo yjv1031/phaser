4d73ea6efb263bfbdf1ed8727985e7d21b3c80f2
yjh <yjv1031gmail.com>
2022-07-18 14:17:57 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          박병준님과 함께하는 즐거운 리액트 세상
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
