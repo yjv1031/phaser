466c39c244b39cc8b70829a1ff8e28a8b19d840b
yjh <yjv1031gmail.com>
2022-07-18 13:47:57 +0900

    디폴트

--- script/first.ts
+++
@@ -1,5 +0,0 @@
-function greeter2(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User1";
-document.body.textContent = greeter2(user);
--- script/test/first.ts
+++
@@ -1,5 +0,0 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User1";
-document.body.textContent = greeter(user);
