ab62e37f2f4e7cccdb111f93044feed40a26e602
yjh <yjv1031gmail.com>
2022-07-19 17:49:08 +0900

    antd 커밋

--- phaser/src/App.tsx
+++ phaser/src/App.tsx
@@ -2,53 +2,89 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useObserver } from 'mobx-react';
-import combindStore from './store/combindStore';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RouteArr from './routes';
 
-function App() {
-  return useObserver(() => (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {
-            RouteArr.map((item) => {
-              return (<Route path={item.path} element={item.element} key={item.path}/>);
-            })
-          }
-        </Routes>
-      </BrowserRouter>
-      {/*  
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ver. mobx
-        </p>
-        {
-          todo.todoData.map((item) => {
-            return (<p key={item.id}>{item.content}</p>);
-          })
-        }
-        {
-          todo2.todoData.map((item) => {
-            return (<p key={item.id}>{item.content}</p>);
-          })
-        }
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+const { Header, Content, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey :any = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+
+const App = () => useObserver(() => (
+  <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{
+            height: '100%',
+            borderRight: 0,
+          }}
+          items={items2}
+        />
+      </Sider>
+      <Layout
+        style={{
+          padding: '0 24px 24px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
         >
-          Learn React
-        </a>
-        <button onClick={() => {todo.addTodo('ff');}}>
-          추가하기
-        </button>
-      </header>
-      */}
-    </div>
-  ));
-}
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+
+          <BrowserRouter>
+            <Routes>
+              {
+                RouteArr.map((item) => {
+                  return (<Route path={item.path} element={item.element} key={item.path}/>);
+                })
+              }
+            </Routes>
+          </BrowserRouter>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
+));
 
 export default App;
