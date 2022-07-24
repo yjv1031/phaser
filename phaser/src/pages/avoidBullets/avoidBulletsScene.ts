29b946145fda0125ef2d7ddffb542887687577e3
yjh <yjv1031@gmail.com>
2022-07-24 23:36:15 +0900

    피직스 적요

--- phaser/src/pages/avoidBullets/avoidBulletsScene.ts
+++ phaser/src/pages/avoidBullets/avoidBulletsScene.ts
@@ -6,25 +6,32 @@ class AvoidBulletsScene extends Phaser.Scene {
     width: 1024,
     height: 768
   }
+  private characterSetting = {
+    speed: 5
+  }
   private character: Phaser.GameObjects.Image|null = null
   private upKey:     Phaser.Input.Keyboard.Key|null = null
   private downKey:   Phaser.Input.Keyboard.Key|null = null
   private leftKey:   Phaser.Input.Keyboard.Key|null = null
   private rightKey:  Phaser.Input.Keyboard.Key|null = null
-  
-  private random: number = 0;
+
+  private a1 : Phaser.GameObjects.Image|null = null
+  private a2 : Phaser.GameObjects.Image|null = null
 
   constructor() {
     super({ key: 'main', active: true })
   }
 
   preload(): void {
-    this.random = Math.random();
-    this.load.image('robot', '/resource/avoidBullets/robot.png');
+    this.load.image('robot', '/phaser/resource/robot.png');
   }
 
   create(): void {
-    this.character = this.add.image(100, 150, 'robot')
+    this.character = this.matter.add.image(100, 150, 'robot').setScale(0.1);
+
+    const a1 = this.matter.add.image(100, 200, 'robot').setScale(0.1).setStatic(true);
+    const a2 = this.matter.add.image(200, 200, 'robot').setScale(0.1).setStatic(true);
+
     //키설정
     this.upKey     = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
     this.downKey   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
@@ -32,13 +39,32 @@ class AvoidBulletsScene extends Phaser.Scene {
     this.rightKey  = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
   }
 
-  update(time: number, delta: number): void {
-    console.log(this.random);
+  aaa(): void {
+    console.log('충돌');
+  }
 
-    if (this.upKey?.isDown)     console.log('위');
-    if (this.downKey?.isDown)   console.log('아래');
-    if (this.leftKey?.isDown)   console.log('왼쪽');
-    if (this.rightKey?.isDown)  console.log('오른쪽');
+  aaa2(): void {
+    console.log('충돌2');
+  }
+
+  update(time: number, delta: number): void {
+    if (this.upKey?.isDown && this.character) {
+      this.character.y -= this.characterSetting.speed;
+    }
+    if (this.downKey?.isDown && this.character) {
+      this.character.y += this.characterSetting.speed;
+    } 
+    if (this.leftKey?.isDown && this.character) {
+      this.character.x -= this.characterSetting.speed;
+    }
+    if (this.rightKey?.isDown && this.character) {
+      this.character.x += this.characterSetting.speed;
+    }
+
+    if(this.character && this.a1 && this.a2) {
+      //this.matter.setCollisionGroup([this.character ,this.a1, this.a2], 1);
+      this.matter.overlap(this.character, [this.a1, this.a2], this.aaa, this.aaa2, this);
+    }
   }
 
   destroy(): void {
@@ -49,7 +75,18 @@ class AvoidBulletsScene extends Phaser.Scene {
       width: this.gameSetting.width,
       height: this.gameSetting.height,
       parent: 'AvoidBulletsGameDiv',
-      scene: [ AvoidBulletsScene ]
+      scene: [ AvoidBulletsScene ],
+      physics: {
+        default: 'matter', // arcade, matter 중 1택
+        // matter 물리엔진 설정
+        matter: {
+          // 월드에 중력 설정
+          gravity: {
+            x: 0,
+            y: 0
+          }
+        }
+      }
     })
     this.gameInstance = game;
   }
