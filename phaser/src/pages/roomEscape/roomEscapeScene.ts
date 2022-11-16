49300a41bac199d894217e7b2bbfba0282b99c31
yjv1031 <yjv1031@gmail.com>
2022-11-16 17:18:37 +0900

    .

--- phaser/src/pages/roomEscape/roomEscapeScene.ts
+++ phaser/src/pages/roomEscape/roomEscapeScene.ts
@@ -1,45 +1,107 @@
 import { Pair } from 'matter';
 import Phaser, { Game } from 'phaser'
+import InputText from 'phaser3-rex-plugins/plugins/inputtext.js';
+import Button from 'phaser3-rex-plugins/plugins/Button.js';
+
+interface ChrFlag {
+  downX: number,
+  downY: number,
+  speed: number,
+  position: String,
+  msg: Phaser.GameObjects.Text|null,
+  msgText: string|undefined,
+  msgTimer: number,
+  msgEnterFlag: boolean
+}
 
 class RoomEscapeScene extends Phaser.Scene {
   private gameInstance: Game|null = null;
   private gameSetting = {
     width: 1024,
-    height: 768
+    height: 768,
+    msgTimer: 200,
+    continueTimer: 200
   }
-  private myCharacter: Phaser.GameObjects.Sprite|null = null
-  private myCharacterFlag = {
+  private myCharacter: Phaser.GameObjects.Sprite|null = null;
+  private inputBtn: Phaser.GameObjects.Image|null = null;
+
+  private myCharacterFlag = <ChrFlag>{
     downX: 300,
     downY: 500,
     speed: 5,
-    position: "3" 
+    position: "3",
+    msg: null,
+    msgText: "",
+    msgTimer: 0,
+    msgEnterFlag: true
   };
+
+  private inputChat: InputText|null = null;
 
   constructor() {
     super({ key: 'RoomEscapeGameMain', active: true })
   }
 
   preload(): void {
-    this.load.image('robot', '/phaser/resource/sopa2.png');
-    this.load.spritesheet("lidia", "/phaser/resource/lidia.png", { frameWidth: 64, frameHeight: 64 });
+   this.load.spritesheet("lidia", "/phaser/resource/lidia.png", { frameWidth: 64, frameHeight: 64 });
+   this.load.image('inputBtn', '/phaser/resource/sopa.png');
   }
   
   create(): void {
     this.createSpriteAnimation();
     //캐릭터 객체 생성
-    //this.myCharacter = this.matter.add.image(300, 500, 'robot', undefined, {label: 'character'}).setScale(1).setInteractive();
     this.myCharacter = this.matter.add.sprite(300, 500, 'lidia', undefined, {label: 'character'}).setScale(1);
-
-    this.myCharacter.on('pointerdown', () => {
-      console.log('비행기 클릭');
-    });
 
     console.log(this.matter.world);
 
+    //말풍선
+    this.myCharacterFlag.msg = this.add.text(this.myCharacter.x, this.myCharacter.y -50, "", {
+        font: "15px Arial",
+        //fill: "#ff0044",
+        align: "center"
+    });
+
+    //채팅 입력창 생성
+    this.inputChat = new InputText(this.scene.scene, 400, this.gameSetting.height - 50, 500, 30, {backgroundColor: '#FFFFFF', type: 'text', color: '#000000'}).setInteractive();
+    this.scene.scene.add.existing(this.inputChat);
+    this.inputChat.on('keydown', (inputText: any, e: any) => { 
+      //엔터입력 취급
+      if(e.code == 'NumpadEnter' || e.code == 'Enter') {
+        if(this.myCharacterFlag.msgEnterFlag) {
+          if(this.inputChat) {
+            this.myCharacterFlag.msgEnterFlag = false;
+            this.myCharacterFlag.msgText = this.inputChat.text;
+            this.myCharacterFlag.msgTimer = this.gameSetting.msgTimer;
+            this.inputChat.setText("");
+            //연속 입력행위를 제한한다
+            setTimeout(() => {
+              this.myCharacterFlag.msgEnterFlag = true;
+            },this.gameSetting.continueTimer);
+          }
+        } else {
+          if(this.inputChat) {
+            this.inputChat.setText("");
+          }
+        }
+      }
+    });
+
+    //채팅데이터 입력 버튼
+    const inputBtn = this.matter.add.sprite(700, this.gameSetting.height - 50, 'inputBtn', undefined, {label: 'inputBtn'}).setScale(0.2).setStatic(true).setInteractive();
+    inputBtn.on('pointerdown', () => {
+      if(this.inputChat) {
+        this.myCharacterFlag.msgText = this.inputChat.text;
+        this.inputChat.setText("");
+        this.myCharacterFlag.msgTimer = this.gameSetting.msgTimer;
+      }
+    });
+    
     //마우스 바닥 클릭 이벤트
     this.input.on('pointerdown', (event: any, param: any) => {
-      this.myCharacterFlag.downX = event.downX;
-      this.myCharacterFlag.downY = event.downY;
+      if(event.downY < this.gameSetting.height - 100){
+        this.myCharacterFlag.downX = event.downX;
+        this.myCharacterFlag.downY = event.downY;
+      }
     });
   }
   
@@ -81,9 +143,25 @@ class RoomEscapeScene extends Phaser.Scene {
           this.myCharacter.anims.stop();
           this.myCharacter.anims.play(String(characterP), true);
         }
+
+        //말풍선이 함께 무빙
+        this.myCharacterFlag.msg?.setX(this.myCharacter.x);
+        this.myCharacterFlag.msg?.setY(this.myCharacter.y - 50);
+        
       } else {
         //정지되어 있는 상태
         this.myCharacter.anims.stop();
+      }
+
+      //채팅내용 동기화
+      if(this.myCharacterFlag.msgText) {
+        if(this.myCharacterFlag.msgTimer > 0) {
+          this.myCharacterFlag.msg?.setText(this.myCharacterFlag.msgText);
+          this.myCharacterFlag.msgTimer -= 1;
+        } else {
+          this.myCharacterFlag.msg?.setText("");
+          this.myCharacterFlag.msgTimer = 0;
+        }
       }
     }
   }
@@ -96,7 +174,11 @@ class RoomEscapeScene extends Phaser.Scene {
       width: this.gameSetting.width,
       height: this.gameSetting.height,
       parent: 'RoomEscapeGameDiv',
+      backgroundColor: "#555555",
       scene: [ RoomEscapeScene ],
+      dom: {
+        createContainer: true
+      },
       physics: {
         default: 'matter', // arcade, matter 중 1택
         // matter 물리엔진 설정
@@ -130,10 +212,6 @@ class RoomEscapeScene extends Phaser.Scene {
     anims.create({
         key: "3", frameRate: 10, repeat: -1,
         frames: this.anims.generateFrameNumbers('lidia', { start: 18, end: 26 }),
-    });
-    anims.create({
-        key: "waiting", frameRate: 3, repeat: -1,
-        frames: this.anims.generateFrameNumbers('lidia', { start: 19, end: 21 }),
     });
   }
 
