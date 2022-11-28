da04c3a4661e6492bda1f915f409d053a1a6203a
yjv1031 <yjv1031@gmail.com>
2022-11-28 17:43:33 +0900

    1

--- phaser/src/pages/roomEscape/roomEscapeConstant.ts
+++ phaser/src/pages/roomEscape/roomEscapeConstant.ts
@@ -17,6 +17,22 @@ const playerSprite: Array<CharacterSheet> = [
     name: 'mario',
     sumnail: 'mario_sum'
   },
+  {
+    name: 'link',
+    sumnail: 'link_sum'
+  },
+  {
+    name: 'w1',
+    sumnail: 'w1_sum'
+  },
+  {
+    name: 'm1',
+    sumnail: 'm1_sum'
+  },
+  {
+    name: 'w2',
+    sumnail: 'w2_sum'
+  },
 ];
 
 const resourcePath: any = {
